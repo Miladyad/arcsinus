9cf3bb877300690f72af6b18ddf6617e55bc321d
Miladyad <73443790+Miladyad@users.noreply.github.com>
2024-09-25 22:23:09 +0300

    Update dadata1-validation.js

--- dadata1-validation.js
+++ dadata1-validation.js
@@ -1,3 +1,15 @@
 if (!$('#error').length) {    
    $('#field2682865').after('<div id="error"></div>'); 
 };
+
+$("#field2682865").on( "click", function( event ) {
+	event.preventDefault();
+    $(this).removeClass('error2'); 
+});
+
+var token = "9a69485053e79fdc1c87703b5489154cccca7d70";
+
+function join(arr /*, separator */) {
+  var separator = arguments.length > 1 ? arguments[1] : ", ";
+  return arr.filter(function(n){return n}).join(separator);
+}
