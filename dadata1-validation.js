c01a0f66c0340ac699985d0421aaacee8d157145
Miladyad <73443790+Miladyad@users.noreply.github.com>
2024-09-25 22:24:27 +0300

    Update dadata1-validation.js

--- dadata1-validation.js
+++ dadata1-validation.js
@@ -13,3 +13,43 @@ function join(arr /*, separator */) {
   var separator = arguments.length > 1 ? arguments[1] : ", ";
   return arr.filter(function(n){return n}).join(separator);
 }
+
+function showCountry(address) {
+	if (address.country) {
+		$("#field2699209").val(address.country);
+	} else 
+    $("#field2699209").val("-");
+}
+
+function showPostalCode(address) {
+	if (address.postal_code) {
+		$("#field2699208").val(address.postal_code);
+	} else 
+    $("#field2699208").val("-");
+}
+
+function showRegion(address) {
+	if (address.region) {
+		$("#field2699211").val(join([
+			join([address.region_type, address.region], " "),
+			join([address.area_type, address.area], " ")
+		]));
+	} else 
+    $("#field2699211").val("-");
+}
+
+function showCity(address) {
+	if (address.city) {
+		$("#field2699218").val(address.city);
+	} else 
+    $("#field2699218").val("-");
+}
+
+function showSettlement(address) {
+	if (address.settlement) {
+		$("#field2699219").val(
+			join([address.settlement_type, address.settlement], " ")
+	  );
+	} else 
+	$("#field2699219").val("-");
+}
