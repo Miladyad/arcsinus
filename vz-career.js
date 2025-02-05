c6987ce503bf02490d03b668d67f44deeab2cb71
Miladyad <73443790+Miladyad@users.noreply.github.com>
2025-02-05 10:27:52 +0300

    Update vz-career.js
    Added script

--- vz-career.js
+++ vz-career.js
@@ -1,4 +1,17 @@
-$(document).ready(function() {
+$(document).ready(function(){
+	$(".agreement-link a").on("click", function(){
+		$(".agreement-block").css("display", "block");
+		$(".agreement-block").animate({"opacity": "1.0"}, 500);
+    
+	});
 
+	$(".agreement-close").on("click", function(){
+		closeBox();
+	});
 
+	function closeBox(){
+		$(".agreement-block").animate({"opacity": "0"}, 500, function(){
+			$(".agreement-block").css("display", "none");
+		});
+	}
 });
