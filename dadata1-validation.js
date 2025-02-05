08f9e88e08120bbd3205097098fbc6dbff6c2862
Miladyad <73443790+Miladyad@users.noreply.github.com>
2025-02-05 13:42:10 +0300

    Update dadata1-validation.js
    2

--- dadata1-validation.js
+++ dadata1-validation.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
+	$('.checkbox input').prop('disabled', true); 
+	
 	$(".agreement-link a").on("click", function(){
 		$(".agreement-block").css("display", "block");
-		$(".agreement-block").animate({"opacity": "1.0"}, 300);
-    
+		$(".agreement-block").animate({"opacity": "1.0"}, 300);    
 	});
 
 	$(".agreement-close").on("click", function(){
@@ -14,4 +15,22 @@ $(document).ready(function(){
 			$(".agreement-block").css("display", "none");
 		});
 	}
+  // Проверяем, достигнут ли конец блока с текстом
+    $('.agreement-content').on('scroll', function() {
+        let scrollTop = $(this).scrollTop();
+        let scrollHeight = $(this).prop('scrollHeight');
+        let outerHeight = $(this).outerHeight();
+
+        if (scrollTop + outerHeight >= scrollHeight) {
+            $('.agreement-button button').prop('disabled', false); // Разблокируем кнопку
+        }
+    });
+  
+  // Обработчик клика на кнопку
+    $('.agreement-button button').on('click', function() {
+        event.preventDefault();
+        closeBox(); // Закрываем всплывающее окно
+		$('.checkbox input').prop('disabled', false);
+        $('.checkbox input').prop('checked', true); // Устанавливаем галочку в чекбоксе
+    });
 });
