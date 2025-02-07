a03fedd5f9ebae93614bef1f6c88d7fd140a57e4
Miladyad <73443790+Miladyad@users.noreply.github.com>
2025-02-07 16:58:10 +0300

    Update vz-career.js
    Изменение кода п.5 тестировщик

--- vz-career.js
+++ vz-career.js
@@ -1,36 +1,48 @@
 $(document).ready(function(){
 	$('.checkbox input').prop('disabled', true); 
+	var checkboxContent = $(".checkbox").html;
 	
+	// show error field under checkbox on click
+	$(".checkbox input").on("click", function(){		
+		$(".checkbox").html(checkboxContent + "<div class='error'>Пожалуйста, прочитайте текст соглашения.</div>");
+		$(".checkbox").addClass("error");    
+	});
+	
+	// show modal window
 	$(".agreement-link a").on("click", function(){
 		$(".agreement-block").css("display", "block");
 		$(".agreement-block").animate({"opacity": "1.0"}, 300);    
 	});
-
+	
+	// close modal window
 	$(".agreement-close").on("click", function(){
 		closeBox();
 	});
-
 	function closeBox(){
 		$(".agreement-block").animate({"opacity": "0"}, 300, function(){
 			$(".agreement-block").css("display", "none");
 		});
 	}
-  // Проверяем, достигнут ли конец блока с текстом
+	
+  // agreement text is read
     $('.agreement-content').on('scroll', function() {
         let scrollTop = $(this).scrollTop();
         let scrollHeight = $(this).prop('scrollHeight');
         let outerHeight = $(this).outerHeight();
 
         if (scrollTop + outerHeight >= scrollHeight) {
-            $('.agreement-button button').prop('disabled', false); // Разблокируем кнопку
+            $('.agreement-button button').prop('disabled', false); // unblocking agreement button
         }
     });
   
-  // Обработчик клика на кнопку
+  // Checking agreement checkbox, removing error
     $('.agreement-button button').on('click', function() {
         event.preventDefault();
-        closeBox(); // Закрываем всплывающее окно
+        closeBox(); 
+		$(".checkbox").html(checkboxContent);
+		$(".checkbox").removeClass("error");
 		$('.checkbox input').prop('disabled', false);
-        $('.checkbox input').prop('checked', true); // Устанавливаем галочку в чекбоксе
+        $('.checkbox input').prop('checked', true); 
+		$('.checkbox input').trigger('click');
     });
 });
